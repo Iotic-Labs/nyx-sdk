fed42f8955a3f35091b5cad41d349ad8d072fe3e
andy blair <andy.blair@iotics.com>
2024-10-21 10:11:00 +0100

    fix: update retry

--- nyx_client/nyx_client/utils.py
+++ nyx_client/nyx_client/utils.py
@@ -39,8 +39,8 @@ def auth_retry(func):
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 401 and not getattr(wrapper, "_retried", False):
                 # Unauthorized error and haven't retried yet
-                self._authorise(refresh=True)
                 wrapper._retried = True
+                self._authorise(refresh=True)
                 return func(self, *args, **kwargs)
             else:
                 # Either it's not an auth error or we've already retried
