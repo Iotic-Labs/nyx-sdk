0338798fc4c1dd8cd29511f0bc489bc3cd85ef03
andy blair <andy.blair@iotics.com>
2024-10-22 09:17:03 +0100

    feat: Move AI related helpers to nyx-extras package
    Co-authored-by: Vilnis Termanis <vilnis.termanis@iotics.com>

---
+++ nyx_extras/nyx_extras/__init__.py
@@ -0,0 +1,20 @@
+# Copyright © 2024 IOTIC LABS LTD. info@iotics.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/Iotic-Labs/nyx-sdk/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ruff: noqa: F401
+
+"""NYC client SDK optional extras for langchain and auto-parsing."""
+
+from nyx_extras.langchain import NyxLangChain
+from nyx_extras.utils import Metadata, Parser, Utils, VectorResult
