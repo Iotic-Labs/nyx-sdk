dbb7acdc335a9532815d86e1a9f2f8fc99940325
andy blair <andy.blair@iotics.com>
2024-10-22 09:15:08 +0100

    feat: Move Nyx-client to be API based, and remove dependencies
    Co-authored-by: Vilnis Termanis <vilnis.termanis@iotics.com>

---
+++ nyx_client/docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "nyx-client"
+copyright = "2024, Iotics"
+author = "Iotics"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "autoapi.extension",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# -- Manually added options for extensions
+
+autodoc_typehints = "description"
+
+autoapi_dirs = ["../nyx_client"]
+autoapi_options = [
+    "members",
+    "inherited-members",
+    "show-inheritance",
+    "special-members",
+    "show-module-summary",
+    "imported-members",
+]
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
---
+++ nyx_client/docs/index.rst
@@ -0,0 +1,17 @@
+nyx-client Documentation
+========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   changelog
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+   :start-line: 1
+   :end-before: </div>
+
+.. include:: ../README.md
+   :parser: myst_parser.sphinx_
+   :start-after: </div>
