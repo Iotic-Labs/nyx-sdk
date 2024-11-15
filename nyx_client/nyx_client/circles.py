cc6f990cd59e3f81ed0b407f282a09d43f15a08c
Vilnis Termanis <vilnis.termanis@iotics.com>
2024-11-15 12:32:49 +0100

    chore(client): Include circles module in docs submodules listing
    - By introducing a module docstring

--- nyx_client/nyx_client/circles.py
+++ nyx_client/nyx_client/circles.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Module for Circles-related (i.e. ACL) definitions in Nyx."""
+
 from dataclasses import asdict, dataclass
 from typing import Any, Sequence
 
